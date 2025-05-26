827eeb7eaa08a794394fd023c0b7f402ccfb97ba
VinhCanCode <sandilove59@gmail.com>
2025-05-26 16:40:48 +0700

    fix gg uri

--- src/app/pages/Auth/OauthLogin/OauthLogin.tsx
+++ src/app/pages/Auth/OauthLogin/OauthLogin.tsx
@@ -5,8 +5,8 @@ export default function OauthLogin() {
   const [params] = useSearchParams()
   const navigate = useNavigate()
   useEffect(() => {
-    const access_token = params.get('access_token')
-    const refresh_token = params.get('refresh_token')
+    const access_token = params.get('access_token') as string
+    const refresh_token = params.get('refresh_token') as string
     localStorage.setItem('access_token', access_token)
     localStorage.setItem('refresh_token', refresh_token)
     navigate('/customer/dashboard')
