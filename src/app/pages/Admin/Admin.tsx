53724d3f2936fd97d3a94b73f3037ae97add7d01
duydev245 <danniduy.work@gmail.com>
2025-06-22 22:20:33 +0700

    CORRECT: Fix layout admin

--- src/app/pages/Admin/Admin.tsx
+++ src/app/pages/Admin/Admin.tsx
@@ -1,16 +1,20 @@
-import { SidebarInset, SidebarProvider } from "@/app/components/ui/sidebar";
-import AdminHeader from "@/app/layouts/AdminLayout/AdminHeader";
-import AdminSidebar from "@/app/layouts/AdminLayout/AdminSidebar";
-import { Outlet } from "react-router-dom";
+import { SidebarInset, SidebarProvider } from '@/app/components/ui/sidebar'
+import AdminHeader from '@/app/layouts/AdminLayout/AdminHeader'
+import AdminSidebar from '@/app/layouts/AdminLayout/AdminSidebar'
+import { Outlet } from 'react-router-dom'
 
 export default function Admin() {
-  return  (
+  return (
     <>
       <SidebarProvider>
         <AdminSidebar />
         <SidebarInset>
           <AdminHeader />
-          <Outlet />
+          <div className='min-h-screen bg-gray-50 p-6'>
+            <div className='max-w-7xl mx-auto space-y-6'>
+              <Outlet />
+            </div>
+          </div>
         </SidebarInset>
       </SidebarProvider>
     </>
